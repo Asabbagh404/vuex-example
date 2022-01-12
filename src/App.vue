2e512f91630d6b35e4f2c2848cbd8d7f896e09b3
Anthony Sabbagh <ash@f4-group.com>
2022-01-12 16:16:46 +0100

    feat: add call to vuex

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="counter">
+    <div class="count">Count: {{count}}</div>
+    <div class="count-btns">
+      <button type="button" class="btn btn-default" @click="countIncrement">increment</button>
+      <button type="button" class="btn btn-default" @click="countDecrement">decrement</button>
     </div>
-    <router-view/>
   </div>
 </template>
+<script>
+import { mapState, mapActions } from 'vuex'
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+export default {
+  computed: mapState([
+    'count'
+  ]),
+  methods: mapActions([
+    'countIncrement',
+    'countDecrement'
+  ])
+}
+</script>
+<style scoped>
+.count {
+  margin: 1em 0;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+.count-btns > .btn {
+    margin-right: 10px;
+  }
 </style>
